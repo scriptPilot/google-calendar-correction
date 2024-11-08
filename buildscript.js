3accada332fd0900462306182d44ea1dbde899f0
scriptPilot <dennis@scheffel.de>
2024-11-08 18:46:53 +0100

    add source and date to build

--- buildscript.js
+++ buildscript.js
@@ -19,6 +19,12 @@ fs.readdir('lib', (err, files) => {
   // Start array of code blocks
   const codeBlocks = []
 
+  // Add date and link
+  const now = new Date()
+  const date = now.toISOString().substr(0, 10)
+  const link = 'https://github.com/scriptPilot/google-calendar-correction'
+  codeBlocks.push(`// Google Calendar Correction, build on ${date}\n// Source: ${link}`)
+
   // Loop files
   files.forEach(file => {
 
